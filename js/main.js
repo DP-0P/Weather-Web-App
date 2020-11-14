33dfb6c03eab1cf10c0564c37e946b552af67abe
Deepak Pattnayak <60617347+DP-4419@users.noreply.github.com>
2020-11-14 13:14:52 +0530

    commenting out

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-var input = document.querySelector('.input_text');
-var main = document.querySelector('#name');
-var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
-var clouds = document.querySelector('.clouds');
-var button= document.querySelector('.submit');
+// var input = document.querySelector('.input_text');
+// var main = document.querySelector('#name');
+// var temp = document.querySelector('.temp');
+// var desc = document.querySelector('.desc');
+// var clouds = document.querySelector('.clouds');
+// var button= document.querySelector('.submit');
 
 
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=57bdfc590f38d2a1d71c3532daee6154')
-.then(response => response.json())
-.then(data => {
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
+// button.addEventListener('click', function(name){
+// fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=57bdfc590f38d2a1d71c3532daee6154')
+// .then(response => response.json())
+// .then(data => {
+//   var tempValue = data['main']['temp'];
+//   var nameValue = data['name'];
+//   var descValue = data['weather'][0]['description'];
 
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
+//   main.innerHTML = nameValue;
+//   desc.innerHTML = "Desc - "+descValue;
+//   temp.innerHTML = "Temp - "+tempValue;
+//   input.value ="";
 
-})
+// })
 
-.catch(err => alert("Wrong city name!"));
-})
+// .catch(err => alert("Wrong city name!"));
+// })
